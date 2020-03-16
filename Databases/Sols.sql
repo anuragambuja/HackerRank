db4419543e10a44ac67f2e8c29c1a94d4a9c72eb
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-03-16 23:54:07 +0530

    Create Sols.sql

---
+++ Databases/Sols.sql
@@ -0,0 +1,9 @@
+-- 1. Basics of Sets and Relations #1
+-- https://www.hackerrank.com/challenges/basics-of-sets-and-relational-algebra-1/problem
+8
+
+-- 2. Basics of Sets and Relations #2
+-- https://www.hackerrank.com/challenges/basics-of-sets-and-relational-algebra-2/problem
+5
+
+-- 3. 
