f7a34e50d12fbecdbc264fe0a4592133ea0b4e19
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-03-22 20:26:05 +0530

    Update Sols.sql

--- SQL/Sols.sql
+++ SQL/Sols.sql
@@ -41,7 +41,70 @@ where rnk =1) b
 where t.submission_date = b.submission_date
 order by submission_date;
 
+-- The Report  https://www.hackerrank.com/challenges/the-report/problem
+
+select case when grade > 7 then s.name
+            else NULL
+        end name, grade, marks
+from grades g, students s
+where s.marks between min_mark and max_mark
+order by grade desc,name, marks asc;
+
+-- Top Competitors  https://www.hackerrank.com/challenges/full-score/problem
+
+select h.hacker_id, h.name
+from submissions s, challenges c, difficulty d, hackers h
+where s.challenge_id = c.challenge_id 
+      and c.difficulty_level = d.difficulty_level
+      and s.score = d.score 
+      and s.hacker_id = h.hacker_id
+group by h.hacker_id, h.name
+having count(*)>1
+order by count(*) desc, h.hacker_id;
+
+-- Ollivander's Inventory  https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
+
+select id, age, coins_needed, power 
+from (select w.id, p.age, w.coins_needed, w.power
+, rank() over (partition by w.power, p.age order by w.coins_needed ) rnk
+from wands w, wands_property p
+where p.is_evil = 0 and p.code = w.code
+) 
+where rnk =1
+order by power desc, age desc;
+
+-- Challenges https://www.hackerrank.com/challenges/challenges/problem
+
+select hacker_id, name, cnt 
+from 
+(select h.hacker_id, h.name, count(1) cnt
+from hackers h, challenges c
+where h.hacker_id = c.hacker_id 
+group by h.hacker_id, h.name) x
+where cnt in (select ct from (select ct, count(*) cst from (select hacker_id, count(*) ct from challenges group by hacker_id) group by ct) where cst =1 ) or cnt in (select max(ct) from (select count(*) ct from challenges group by hacker_id))
+order by cnt desc, hacker_id asc;
+
+-- Contest Leaderboard  https://www.hackerrank.com/challenges/contest-leaderboard/problem
+
+select s.hacker_id, h.name, s.tot
+from 
+(select hacker_id, sum(scores) tot
+ from 
+(select hacker_id, max(score) scores
+from submissions
+group by hacker_id, challenge_id)
+group by hacker_id) s, hackers h
+where (s.hacker_id = h.hacker_id) and (s.tot > 0)
+order by s.tot desc, s.hacker_id;
+
 -- 
+
+
+
+
+
+
+
 
 
 
