bfa3ebb4097f35132cb0039b6a8d838bee63a2c6
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 17:31:14 +0530

    Update Medium.sql

--- SQL/Medium.sql
+++ SQL/Medium.sql
@@ -1,9 +1,9 @@
---The_PADS   https://www.hackerrank.com/challenges/the-pads/problem
+--1. The_PADS   https://www.hackerrank.com/challenges/the-pads/problem
 
 select concat(concat(name,'('),concat(substr(occupation,1,1),')')) from occupations order by name asc;
 select concat(concat('There are a total of ', count(1)),concat(' ',concat(lower(occupation), 's.'))) from occupations group by occupation order by count(occupation), occupation;
 
--- Occupations  https://www.hackerrank.com/challenges/occupations/problem
+--2. Occupations  https://www.hackerrank.com/challenges/occupations/problem
 
 select doc, prof, sin, act from 
 (select row_number() over (partition by occupation order by name) rn,
@@ -16,7 +16,7 @@ pivot (max(name) for occupation in ('Doctor' as doc,
                                     'Actor' as act))
 order by rn;
 
--- Binary_Tree_Nodes  https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+--3. Binary_Tree_Nodes  https://www.hackerrank.com/challenges/binary-search-tree-1/problem
 
 select N,case
     when P is null then 'Root'
@@ -26,20 +26,20 @@ select N,case
 from BST
 order by N;
 
--- Weather_Observation_Station_18   https://www.hackerrank.com/challenges/weather-observation-station-18/problem 
+--4. Weather_Observation_Station_18   https://www.hackerrank.com/challenges/weather-observation-station-18/problem 
 
 select round((max(LAT_N) - min(LAT_N) + max(LONG_W) - min(LONG_W)), 4) from station;
 
--- Weather_Observation_Station_19 https://www.hackerrank.com/challenges/weather-observation-station-19/problem
+--5. Weather_Observation_Station_19 https://www.hackerrank.com/challenges/weather-observation-station-19/problem
 
 select round(sqrt((a-b)*(a-b)+(c-d)*(c-d)), 4)
 from (select min(LAT_N) a, max(LAT_N) b, min(LONG_W) c, max(LONG_W) d from station);
 
--- Weather_Observation_Station_20   https://www.hackerrank.com/challenges/weather-observation-station-20/problem
+--6. Weather_Observation_Station_20   https://www.hackerrank.com/challenges/weather-observation-station-20/problem
 
 select round(median(LAT_N),4) from station;
 
--- New Companies -  https://www.hackerrank.com/challenges/the-company/problem
+--7. New Companies -  https://www.hackerrank.com/challenges/the-company/problem
 
 select c.company_code, c.founder, count(distinct l.lead_manager_code),  count(distinct s.senior_manager_code),  count(distinct m.manager_code),  count(distinct e.employee_code) 
 from company c
@@ -53,7 +53,7 @@ left join employee e
 on m.manager_code = e.manager_code
 group by c.company_code, c.founder order by c.company_code;
 
--- The Report  https://www.hackerrank.com/challenges/the-report/problem
+--8. The Report  https://www.hackerrank.com/challenges/the-report/problem
 
 select case when grade > 7 then s.name
             else NULL
@@ -62,7 +62,7 @@ from grades g, students s
 where s.marks between min_mark and max_mark
 order by grade desc,name, marks asc;
 
--- Top Competitors  https://www.hackerrank.com/challenges/full-score/problem
+--9. Top Competitors  https://www.hackerrank.com/challenges/full-score/problem
 
 select h.hacker_id, h.name
 from submissions s, challenges c, difficulty d, hackers h
@@ -74,7 +74,7 @@ group by h.hacker_id, h.name
 having count(*)>1
 order by count(*) desc, h.hacker_id;
 
--- Ollivander's Inventory  https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
+--10. Ollivander's Inventory  https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
 
 select id, age, coins_needed, power 
 from (select w.id, p.age, w.coins_needed, w.power
@@ -85,7 +85,7 @@ where p.is_evil = 0 and p.code = w.code
 where rnk =1
 order by power desc, age desc;
 
--- Challenges https://www.hackerrank.com/challenges/challenges/problem
+--11. Challenges https://www.hackerrank.com/challenges/challenges/problem
 
 select hacker_id, name, cnt 
 from 
@@ -96,7 +96,7 @@ group by h.hacker_id, h.name) x
 where cnt in (select ct from (select ct, count(*) cst from (select hacker_id, count(*) ct from challenges group by hacker_id) group by ct) where cst =1 ) or cnt in (select max(ct) from (select count(*) ct from challenges group by hacker_id))
 order by cnt desc, hacker_id asc;
 
--- Contest Leaderboard  https://www.hackerrank.com/challenges/contest-leaderboard/problem
+--12. Contest Leaderboard  https://www.hackerrank.com/challenges/contest-leaderboard/problem
 
 select s.hacker_id, h.name, s.tot
 from 
@@ -109,7 +109,7 @@ group by hacker_id) s, hackers h
 where (s.hacker_id = h.hacker_id) and (s.tot > 0)
 order by s.tot desc, s.hacker_id;
 
--- SQL Project Planning   https://www.hackerrank.com/challenges/sql-projects/problem
+--13. SQL Project Planning   https://www.hackerrank.com/challenges/sql-projects/problem
 
 SELECT start_date, MIN(end_date)
 FROM 
@@ -119,7 +119,7 @@ where start_date < end_date
 GROUP BY start_date
 ORDER BY to_date(start_date) - to_date(MIN(end_date)) desc, start_date;
 
---  Placements  https://www.hackerrank.com/challenges/placements/problem
+--14.  Placements  https://www.hackerrank.com/challenges/placements/problem
 
 select name
 from (
@@ -133,7 +133,7 @@ where st.fid = pt.id
 where fsal > sal 
 order by fsal;
      
---  Symmetric Pairs  https://www.hackerrank.com/challenges/symmetric-pairs/problem
+--15.  Symmetric Pairs  https://www.hackerrank.com/challenges/symmetric-pairs/problem
 
 select a.x, a.y
 from 
@@ -142,7 +142,7 @@ from
 where a.x=b.y and a.y=b.x and a.rn < b.rn
 order by a.x;
 
-
+--16. Print Prime Numbers  https://www.hackerrank.com/challenges/print-prime-numbers/problem
 
 
 
