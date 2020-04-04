c8b2773c13d168b6bd0f0541377195ddb1bf3db0
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 17:23:14 +0530

    Delete Binary_Tree_Nodes.sql

--- SQL/Binary_Tree_Nodes.sql
+++
@@ -1,9 +0,0 @@
--- https://www.hackerrank.com/challenges/binary-search-tree-1/problem
-
-select N,case
-    when P is null then 'Root'
-    when N in (select distinct P from BST) then 'Inner'
-    else 'Leaf'
-    end val
-from BST
-order by N;
