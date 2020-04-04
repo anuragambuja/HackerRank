9283c19f75365489eefb7335697182ded8faf467
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 17:45:04 +0530

    Delete Weather_Observation_Station_8.sql

--- SQL/Weather_Observation_Station_8.sql
+++
@@ -1,6 +0,0 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-8/problem
-
-select distinct city from station where regexp_like(lower(substr(city,length(city),1)),'[aeiou]') and regexp_like(lower(substr(city,1,1)),'[aeiou]');
-
-
-select distinct city from station where lower(substr(city,length(city),1)) in ('a','e','i','o','u') and lower(substr(city,1,1)) in ('a','e','i','o','u');
