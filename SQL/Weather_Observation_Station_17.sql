c222669ea16ac9db93cc3f2dd6f4f32eab3a75c3
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 18:00:05 +0530

    Delete Weather_Observation_Station_17.sql

--- SQL/Weather_Observation_Station_17.sql
+++
@@ -1,3 +0,0 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-17/problem
-
-select round(LONG_W,4) from station where LAT_N = (select min(LAT_N) from station where LAT_N > 38.7780);
