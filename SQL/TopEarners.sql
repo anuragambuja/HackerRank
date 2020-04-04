df7627f96a6946888d31f066c6e9c0714b7e7bee
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 17:55:38 +0530

    Delete TopEarners.sql

--- SQL/TopEarners.sql
+++
@@ -1,3 +0,0 @@
--- https://www.hackerrank.com/challenges/earnings-of-employees/problem
-
-select salary*months, count(1) from employee where salary*months in (select max(salary*months) m_earn from employee) group by salary*months;
