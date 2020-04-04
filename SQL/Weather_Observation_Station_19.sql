d6fc33ba8bdccfb482d7a22af34507f138ff3cec
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 17:25:12 +0530

    Delete Weather_Observation_Station_19.sql

--- SQL/Weather_Observation_Station_19.sql
+++
@@ -1,4 +0,0 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-19/problem
-
-select round(sqrt((a-b)*(a-b)+(c-d)*(c-d)), 4)
-from (select min(LAT_N) a, max(LAT_N) b, min(LONG_W) c, max(LONG_W) d from station);
