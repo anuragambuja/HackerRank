7777681970d5146ca4315a51cbdd58eac467bf78
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 18:01:13 +0530

    Delete Asian_Population.sql

--- SQL/Asian_Population.sql
+++
@@ -1,3 +0,0 @@
--- https://www.hackerrank.com/challenges/asian-population/problem
-
-select sum(population) from city where countrycode in (select code from country where lower(continent) = 'asia');
