42419a03e9e712618af5d03ce5e35209e3c8fd92
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 18:01:41 +0530

    Delete Average Population_of_Each_Continent.sql

--- SQL/Average Population_of_Each_Continent.sql
+++
@@ -1,6 +0,0 @@
--- https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
-
-select c.Continent, floor(avg(ci.population))
-from country c, city ci
-where c.code=ci.CountryCode
-group by c.continent;
