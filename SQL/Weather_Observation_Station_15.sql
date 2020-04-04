61d450f45ecdce177f3c72a34239901d2f898a0c
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-04 17:59:58 +0530

    Delete Weather_Observation_Station_15.sql

--- SQL/Weather_Observation_Station_15.sql
+++
@@ -1,3 +0,0 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-15/problem
-
-select round(LONG_W,4) from station where LAT_N in (select max(LAT_N) from station where LAT_N < 137.2345);
