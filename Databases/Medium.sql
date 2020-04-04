5774c6b77d017b7c218195ec354f10ec027cff25
Anurag Ambuja <anurag.ambuja@gmail.com>
2020-04-05 01:42:50 +0530

    Update Medium.sql

--- Databases/Medium.sql
+++ Databases/Medium.sql
@@ -1,1 +1,24 @@
+-- 1. Databases - Natural Joins  https://www.hackerrank.com/challenges/databases-natural-joins/problem
+50
+1
+6
+
+-- 2. Databases - Differences   https://www.hackerrank.com/challenges/databases-differences/problem
+2
+3
+ 
+-- 3. Databases - Keys   https://www.hackerrank.com/challenges/databases-keys/problem
+bookname
+
+-- 4. Databases - Relational Calculus  https://www.hackerrank.com/challenges/databases-relational-calculus/problem
+19
+
+∀ = forall (https://en.wikipedia.org/wiki/Turned_A)
+| = such that (https://en.wikipedia.org/wiki/Set-builder_notation)
+∧ = logical conjunction (and)(https://en.wikipedia.org/wiki/List_of_logic_symbols)
+→ = is a function from(https://math.stackexchange.com/questions/1740154/different-arrows-in-set-theory-rightarrow-and-mapsto)
+
+-- 5. Querying XML Datastores with XPath - 8   https://www.hackerrank.com/challenges/querying-xml-datastores-with-xpath-8/problem
+puts doc.elements.each("string-length(collection/movie[2]/description/text())")
+puts XPath.match(doc,"string-length(collection/movie[2]/description)")
 
